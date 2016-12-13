3dcbade8b8c301c22695d91438d6c5c9c3a63762
Thijs Thijssen <thijs.thijssen1@hotmail.com>
2016-12-13 20:25:55 +0100

    Fixed DML so it matches PDM

--- database/DDL/DDL Tyskie Webshop.sql
+++ database/DDL/DDL Tyskie Webshop.sql
@@ -2,17 +2,74 @@ USE master
 GO
 
 IF DB_ID('TyskieWebshop') IS NOT NULL
-BEGIN
 	DROP DATABASE TyskieWebshop
-END
 GO
 
 CREATE DATABASE TyskieWebshop
 GO
 
-CREATE TABLE test (
-	userName	VARCHAR(50),
-	password	VARCHAR(50),
-	salt		char(16)
+USE TyskieWebshop
+GO
+
+CREATE TABLE Account (
+	gebruikersnaam	VARCHAR(50)		NOT NULL,
+	afrekenCode		VARCHAR(10)		NULL,
+	email			VARCHAR(50)		NOT NULL,
+	wachtwoord		CHAR(128)		NOT NULL,
+	salt			CHAR(128)		NOT NULL,
+	postcode		VARCHAR(10)		NULL,
+	telefoonnummer	VARCHAR(25)		NULL,
+	CONSTRAINT PK_Account PRIMARY KEY (gebruikersnaam)
 )
+GO
+
+CREATE TABLE Winkelmandje (
+	afrekenCode		VARCHAR(10)		NOT NULL,
+	gebruikersnaam	VARCHAR(50)		NOT NULL,
+	totaalprijs		INT				NOT NULL,
+	CONSTRAINT PK_Winkelmandje PRIMARY KEY (afrekenCode)
+)
+GO
+
+CREATE TABLE ProductInWinkelmandje (
+	afrekenCode		VARCHAR(10)		NOT NULL,
+	productCode		VARCHAR(10)		NOT NULL,
+	aantalProducten	INT				NOT NULL,
+	CONSTRAINT PK_ProductInWinkelmandje PRIMARY KEY (afrekenCode, productCode, aantalProducten)
+)
+GO
+
+CREATE TABLE Product (
+	productCode		VARCHAR(10)		NOT NULL,
+	productNaam		VARCHAR(50)		NOT NULL,
+	productPrijs	INT				NOT NULL,
+	CONSTRAINT PK_Product PRIMARY KEY (productCode)
+)
+GO
+
+CREATE TABLE Voorraad (
+	productCode		VARCHAR(10)		NOT NULL,
+	aantalVoorraad	INT				NOT NULL,
+	CONSTRAINT PK_Voorraad PRIMARY KEY (productCode)
+)
+GO
+
+ALTER TABLE Account
+ADD CONSTRAINT FK_Account_Winkelmandje FOREIGN KEY (afrekenCode) REFERENCES Winkelmandje(afrekenCode)
+GO
+
+ALTER TABLE Winkelmandje
+ADD CONSTRAINT FK_Winkelmandje_Account FOREIGN KEY (gebruikersnaam) REFERENCES Account(gebruikersnaam)
+GO
+
+ALTER TABLE ProductInWinkelmandje
+ADD CONSTRAINT FK_ProductInWinkelmandje_Winkelmandje FOREIGN KEY (afrekenCode) REFERENCES Winkelmandje(afrekenCode)
+GO
+
+ALTER TABLE ProductInWinkelmandje
+ADD CONSTRAINT FK_ProductInWinkelmandje_Product FOREIGN KEY (productCode) REFERENCES Product(productCode)
+GO
+
+ALTER TABLE Voorraad
+ADD CONSTRAINT FK_Voorraad_Product FOREIGN KEY (productCode) REFERENCES Product(productCode)
 GO
